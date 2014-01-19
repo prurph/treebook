a09946b982f838b46858eaa2bded10f7460c621c
Prescott Murphy <prurph@gmail.com>
2014-01-18 19:53:38 -0500

    Added devise and set up a basic sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Treebook::Application.routes.draw do
+  devise_for :users
   resources :statuses
+  root to: 'statuses#index' # Sets root URL as requested by rails generate devise:install
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
